a4d6fb2441f252613627c71aba67320484fe9c1e
Gutemberg (a.k.a StGuten) <saddam.gutem@gmail.com>
2021-12-26 10:13:43 -0300

    adicionado configurações para utlização do heroku

--- run.js
+++ run.js
@@ -11,7 +11,7 @@ carregarRecompensas();
 client.on("redeem", async (channel, username, rewardType, tags, message) => {
     switch (rewardType) {
         case "814fd7c9-be2a-40b6-bcf6-d9f6d3738f01":
-            if(seBot.adicionarpontos(username, 10)) seBot.falar(username + ' ganhou 10 pontos!!!!');            
+            if(seBot.adicionarpontos(username, 250)) seBot.falar('@'+ username + ' os frames foram adicionados a sua conta.');            
             break;
     }
 });
